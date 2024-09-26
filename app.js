43ad4768b126932c424251c96ce1198454987573
khadir <tolibxonovm@gmail.com>
2024-09-26 15:57:39 +0500

    homework

---
+++ app.js
@@ -0,0 +1,408 @@
+/* 1- misol */
+
+// const num = prompt("hohlagan sonni kiriting");
+// if (num < 0) {
+//   console.log("bu son mamfiy son");
+// } else {
+//   console.log("bu son musbat son");
+// }
+
+/* 2- misol */
+
+// const num = prompt("hohlagan sonni kiriting");
+// if (num % 2 == 1) {
+//   console.log("bu son toq son ");
+// } else {
+//   console.log("bu son juft son");
+// }
+
+/* 3- misol */
+
+// const num = prompt("hohlagan sonni kiriting");
+// if (num % 2 == 0) {
+//   console.log("bu son juft son ");
+// } else {
+//   console.log("bu son toq son");
+// }
+
+/* 4- misol */
+
+// const num1 = prompt("birinchi sonni kiiriting ");
+// const num2 = prompt("ikkinchi sonni kiiriting ");
+
+// if (num1 > 2 && num2 <= 3) {
+//   console.log("ha");
+// } else {
+//   console.log("yoq");
+// }
+
+/* 5- misol */
+
+// const num1 = prompt("birinchi sonni kiiriting ");
+// const num2 = prompt("ikkinchi sonni kiiriting ");
+
+// if (num1 >= 0 || num2 < -2) {
+//   console.log("ha");
+// } else {
+//   console.log("yoq");
+// }
+
+/* 6- misol */
+
+// const num1 = prompt("birinchi sonni kiiriting ");
+// const num2 = prompt("ikkinchi sonni kiiriting ");
+// const num3 = prompt("uchinchi sonni kiiriting ");
+
+// if (num1 <= num2 && num2 <= num3) {
+//   console.log("Ha");
+// } else {
+//   console.log("Yo'q");
+// }
+
+/* 7- misol */
+// const a = prompt("birinchi sonni kiiriting ");
+// const b = prompt("ikkinchi sonni kiiriting ");
+// const c = prompt("uchinchi sonni kiiriting ");
+
+// if ((a > b && b > c) || (c > b && b > a)) {
+//   console.log("b son ortada yotadi ");
+// } else {
+//   console.log("Yo'q o'rtasidamas");
+// }
+
+/* 8- misol */
+
+// const a = prompt("birinchi sonni kiiriting ");
+// const b = prompt("ikkinchi sonni kiiriting ");
+
+// if (a % 2 == 1 && b % 2 == 1) {
+//   console.log("ha");
+// } else {
+//   console.log("yoq");
+// }
+
+/* 9- misol */
+
+// const a = prompt("birinchi sonni kiiriting ");
+// const b = prompt("ikkinchi sonni kiiriting ");
+
+// if (a % 2 == 1 || b % 2 == 1) {
+//   console.log("ha bittasi toq");
+// } else {
+//   console.log("yoq birortasi ham toq son emas");
+// }
+
+/* 10- misol */
+
+// const a = prompt("birinchi sonni kiiriting ");
+// const b = prompt("ikkinchi sonni kiiriting ");
+
+// if (a % 2 == 1) {
+//   console.log("ha bittasi toq");
+// } else if (b % 2 == 1) {
+//   console.log("ha bittasi toq");
+// } else {
+//   console.log("yoq birortasi ham toq son emas");
+// }
+
+/* 11- misol */
+
+// const a = prompt("birinchi sonni kiiriting ");
+// const b = prompt("ikkinchi sonni kiiriting ");
+
+// if ((a % 2 == 1 && b % 2 == 1) || (a % 2 == 0 && b % 2 == 0)) {
+//   console.log(" TRUE,a b son toq son yoki juft son ");
+// } else {
+//   console.log("FALSE, a va b sonlari toq va juft sonlardan iborat");
+// }
+
+/* 12- misol */
+
+// const a = prompt("birinchi sonni kiiriting ");
+// const b = prompt("ikkinchi sonni kiiriting ");
+// const c = prompt("uchinchi sonni kiiriting ");
+
+// if (a >= 0 && b >= 0 && c >= 0) {
+//   console.log("A B C har biri musbat ");
+// } else {
+//   console.log("A B C har biri musbat EMAS");
+// }
+
+/* 13- misol */
+
+// const a = prompt("birinchi sonni kiiriting ");
+// const b = prompt("ikkinchi sonni kiiriting ");
+// const c = prompt("uchinchi sonni kiiriting ");
+
+// if (a >= 0 || b >= 0 || c >= 0) {
+//   console.log("A B C sonlaridan bittasi bolsa ham musbat ");
+// } else {
+//   console.log("A B C sonlaridan bittasi bolsa ham musbat  EMAS");
+// }
+
+/* 14- misol */
+
+// const a = prompt("birinchi sonni kiiriting ");
+// const b = prompt("ikkinchi sonni kiiriting ");
+// const c = prompt("uchinchi sonni kiiriting ");
+
+// if (a > 0) {
+//   console.log("musbat  son bor");
+// } else if (b > 0) {
+//   console.log("musbat  son bor");
+// } else if (c > 0) {
+//   console.log("musbat  son bor");
+// } else {
+//   console.log("musbat  son yoq");
+// }
+
+/* 15- misol */
+
+// const a = prompt("birinchi sonni kiiriting ");
+// const b = prompt("ikkinchi sonni kiiriting ");
+// const c = prompt("uchinchi sonni kiiriting ");
+
+// if (a > 0 && b > 0) {
+//   console.log(" 2 ta musbat  son bor");
+// } else if (b > 0 && c > 0) {
+//   console.log("2 ta musbat  son bor");
+// } else if (c > 0 && a > 0) {
+//   console.log("2 ta musbat  son bor");
+// } else {
+//   console.log("musbat  son yoq");
+// }
+
+/* 16- misol */
+
+// const a = prompt("sonni kiriting ");
+
+// if (a > 0) {
+//   console.log(Number(a) + 1);
+// } else {
+//   console.log("manifiy son kiritdingiz");
+// }
+
+/* 17- misol */
+
+// const a = prompt("sonni kiriting ");
+
+// if (a > 0) {
+//   console.log(Number(a) + 1);
+// } else {
+//   console.log(Number(a) - 2);
+// }
+
+/* 18- misol */
+
+// let a = prompt("sonni kiriting ");
+
+// if (a == 0) {
+//   a = 10;
+//   console.log(a);
+// } else if (a > 0) {
+//   console.log(Number(a) + 1);
+// } else {
+//   console.log(Number(a) - 2);
+// }
+
+/* 19 - misol */
+
+// const a = prompt("birinchi sonni kiiriting ");
+// const b = prompt("ikkinchi sonni kiiriting ");
+// const c = prompt("uchinchi sonni kiiriting ");
+// let countNumber = 0;
+
+// if (a > 0) {
+//   countNumber++;
+// }
+// if (b > 0) {
+//   countNumber++;
+// }
+// if (c > 0) {
+//   countNumber++;
+// }
+// console.log(`musbat sonlar soni ${countNumber} ta`);
+
+/* 20 - misol */
+
+// const a = prompt("birinchi sonni kiiriting ");
+// const b = prompt("ikkinchi sonni kiiriting ");
+// const c = prompt("uchinchi sonni kiiriting ");
+// let countNumber = 0;
+// let negative = 0;
+
+// if (a > 0) {
+//   countNumber++;
+// } else if (a < 0) {
+//   negative++;
+// }
+// if (b > 0) {
+//   countNumber++;
+// } else if (b < 0) {
+//   negative++;
+// }
+// if (c > 0) {
+//   countNumber++;
+// } else if (c < 0) {
+//   negative++;
+// }
+
+// console.log(`musbat sonlar soni ${countNumber} ta`);
+// console.log(`manfiy sonlar soni ${negative} ta`);
+
+/* 21 - misol */
+
+// const a = prompt("a sonni kiriting ");
+// const b = prompt("b  sonni kiriting ");
+
+// if (a > b) {
+//   console.log("Katta son " + a);
+// } else if (b > a) {
+//   console.log("Katta son " + b);
+// } else {
+//   console.log("Ikkala son teng ");
+// }
+
+/* 22 - misol */
+
+// const a = prompt("a sonni kiriting ");
+// const b = prompt("b  sonni kiriting ");
+
+// if (a < b) {
+//   console.log("a soni kichik tartib raqami : 1");
+// } else if (b < a) {
+//   console.log("b soni kichik tartib raqami : 2");
+// } else {
+//   console.log("Ikkala son teng ");
+// }
+
+/* 23 - misol */
+
+// const a = prompt("a sonni kiriting ");
+// const b = prompt("b  sonni kiriting ");
+
+// if (a < b) {
+//   console.log("b soni katta son  " + b);
+//   console.log("a soni kichik son  " + a);
+// } else if (b < a) {
+//   console.log("a soni katta son  " + a);
+//   console.log("b soni kichik son  " + b);
+// } else {
+//   console.log("Ikkala son teng ");
+// }
+
+/* 24 - misol */
+
+// let a = parseFloat(prompt("Birinchi sonni kiriting  "));
+// let b = parseFloat(prompt("Ikkinchi sonni kiriting  "));
+
+// if (a > b) {
+//   a = a + b;
+//   b = a - b;
+//   a = a - b;
+// }
+// console.log("a kichik son: " + a);
+// console.log("b katta son: " + b);
+
+/* 25 - misol */
+
+// let a = prompt("a sonni kiriting ");
+// let b = prompt("b  sonni kiriting ");
+
+// if (a != b) {
+//   console.log(Number(a) + Number(b));
+// } else {
+//   a = 0;
+//   b = 0;
+//   console.log(a + b);
+// }
+
+/* 26 - misol */
+
+// let a = prompt("a sonni kiriting ");
+// let b = prompt("b  sonni kiriting ");
+
+// if (a != b) {
+//   if (a > b) {
+//     console.log("a soni katta son " + a);
+//   } else {
+//     console.log("b soni katta son " + b);
+//   }
+// } else {
+//   console.log("Ikkala son teng ");
+// }
+
+/* 27 - misol */
+
+// const a = prompt("birinchi sonni kiiriting ");
+// const b = prompt("ikkinchi sonni kiiriting ");
+// const c = prompt("uchinchi sonni kiiriting ");
+
+// if (a < b && a < c) {
+//   console.log("kichik son " + a);
+// } else if (b < a && b < c) {
+//   console.log("kichik son " + b);
+// } else if (c < a && c < b) {
+//   console.log("kichik son " + c);
+// } else {
+//   console.log("2ta yoki 3 ta  son teng ");
+// }
+
+/* 28 - misol */
+
+// const a = prompt("birinchi sonni kiiriting ");
+// const b = prompt("ikkinchi sonni kiiriting ");
+// const c = prompt("uchinchi sonni kiiriting ");
+
+// if ((a > b && b > c) || (c > b && b > a)) {
+//   console.log(" ortacha son  " + b);
+// } else if ((a < b && a > c) || (c < b && b > a)) {
+//   console.log("ortacha son " + a);
+// } else if ((c > b && c < a) || (c > a && c < b)) {
+//   console.log("ortacha son " + c);
+// } else {
+//   console.log("2ta yoki 3 ta son birxil ");
+// }
+
+/* 29 - misol */
+
+// const a = prompt("Birinchi sonni kiriting  ");
+// const b = prompt("Ikkinchi sonni kiriting  ");
+// const c = prompt("Uchinchi sonni kiriting  ");
+
+// let min, max;
+// if (a <= b && a <= c) {
+//   min = a;
+// } else if (b <= a && b <= c) {
+//   min = b;
+// } else {
+//   min = c;
+// }
+// if (a >= b && a >= c) {
+//   max = a;
+// } else if (b >= a && b >= c) {
+//   max = b;
+// } else {
+//   max = c;
+// }
+// console.log("Eng kichik son  " + min);
+// console.log("Eng katta son  " + max);
+
+/* 30 - misol */
+
+// const a = prompt("Birinchi sonni kiriting  ");
+// const b = prompt("Ikkinchi sonni kiriting  ");
+// const c = prompt("Uchinchi sonni kiriting  ");
+// let ab = Number(a) + Number(b);
+// let ac = Number(a) + Number(c);
+// let bc = Number(b) + Number(c);
+
+// if (ab > ac && ab > bc) {
+//   console.log("eng katta yigindi :" + ab);
+// } else if (ac > bc && ac > ab) {
+//   console.log("eng katta yigindi :" + ac);
+// } else if (bc > ac && bc > ab) {
+//   console.log(" eng katta yigindi : " + bc);
+// } else {
+//   console.log("2 ta yoki 3ta son teng bolishi mumkin ");
+// }
